2a789241e554fa625b1d028bd6a08db4f64ee65d
charlottechalmer <charlotte.taylor7693@gmail.com>
2017-03-06 11:48:23 -0700

    inital testing

---
+++ tests/app.js
@@ -0,0 +1,11 @@
+/*jshint esversion:6*/
+var chai = require('chai');
+
+
+//set up testing on the server
+
+describe('tests', () => {
+	it('should return true', () => {
+		assert.isTrue(true);
+	});
+});
