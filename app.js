8ec315805ce425613ac38bb588e46b998c7ab45e
charlottechalmer <charlotte.taylor7693@gmail.com>
2017-03-06 12:11:59 -0700

    testing

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static('public'));
+
 app.listen(8000, () => {
 	console.log('server started on port 8000');
 });
